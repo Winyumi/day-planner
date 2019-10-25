7bdcdf692388c5f234d353629f97429dde02ee46
Winyumi <winguy2003@hotmail.com>
2019-10-25 14:10:14 -0400

    clean up logs and comments

--- assets/js/planner.js
+++ assets/js/planner.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     var workHours = ["9 AM","10 AM","11 AM","12 PM","1 PM","2 PM","3 PM","4 PM","5 PM"];
 
     if (window.location.search.substring(1).match(/all/gi)) {
-        var workHours = ["12 AM","1 AM","2 AM","3 AM","4 AM","5 AM","6 AM","7 AM","8 AM","9 AM","10 AM","11 AM","12 PM","1 PM","2 PM","3 PM","4 PM","5 PM","6 PM","7 PM","8 PM","9 PM","10 PM","11 PM"]; // For debugging purposes
+        var workHours = ["12 AM","1 AM","2 AM","3 AM","4 AM","5 AM","6 AM","7 AM","8 AM","9 AM","10 AM","11 AM","12 PM","1 PM","2 PM","3 PM","4 PM","5 PM","6 PM","7 PM","8 PM","9 PM","10 PM","11 PM"];
     }
 
     var agendaItems = {};
@@ -53,7 +53,6 @@ $(document).ready(function() {
     function getAgenda() {
         if (localStorage.getItem("agenda")) {
             agendaItems = JSON.parse(localStorage.getItem("agenda"));
-            console.log(agendaItems);
         }
     }
 
